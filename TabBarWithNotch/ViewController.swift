9d382f7c92b87bf49033bb976133d0a243715513
Johnstone Ananda <johnolwamba@gmail.com>
2020-04-08 20:16:35 +0300

    Initial Commit

---
+++ TabBarWithNotch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarWithNotch
+//
+//  Created by Johnstone Ananda on 08/04/2020.
+//  Copyright © 2020 Johnstone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
